80a5d440429f9292c507e16456ebcec0d48ecb65
Lucas Ortiz <123038170+espaarg@users.noreply.github.com>
2024-06-05 11:57:31 -0300

    a

---
+++ src/main/java/org/example/Prueba.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Prueba {
+
+    public Prueba(){
+
+    }
+
+    public double suma (double a, double b){
+        return a+b;
+    }
+
+}
---
+++ src/test/java/org/example/PruebaTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PruebaTest {
+
+    @Test
+    public void testSumaPositivo() {
+        Prueba prueba = new Prueba();
+        double resultado = prueba.suma(2.5, 3.8);
+        double esperado = 6.3;
+
+        // Comprueba si el resultado es igual al valor esperado con un delta de 0.01 para tolerar redondeos
+        org.junit.jupiter.api.Assertions.assertEquals(esperado, resultado, 0.01);
+    }
+}
