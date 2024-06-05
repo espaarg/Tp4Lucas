48946031295c6a034c3c405a2205384bf8cf220d
Lucas Ortiz <123038170+espaarg@users.noreply.github.com>
2024-06-05 12:01:17 -0300

    Update PruebaTest.java

--- src/test/java/org/example/PruebaTest.java
+++ src/test/java/org/example/PruebaTest.java
@@ -10,7 +10,7 @@ public class PruebaTest {
     public void testSumaPositivo() {
         Prueba prueba = new Prueba();
         double resultado = prueba.suma(2.5, 3.8);
-        double esperado = 6.3;
+        double esperado = 6.5;
 
         // Comprueba si el resultado es igual al valor esperado con un delta de 0.01 para tolerar redondeos
         org.junit.jupiter.api.Assertions.assertEquals(esperado, resultado, 0.01);
