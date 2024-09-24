b616c9941b1f7d9ebd814512c19858ed1066c0dc
Lucas Ortiz <123038170+espaarg@users.noreply.github.com>
2024-09-23 22:05:12 -0300

    Update CarritoIT.java

--- src/test/java/org/example/entidades/CarritoIT.java
+++ src/test/java/org/example/entidades/CarritoIT.java
@@ -2,7 +2,7 @@ package org.example.entidades;
 
 import org.junit.jupiter.api.Test;
 
-public class CarritoIntegrationTest {
+public class CarritoIT {
 
     @Test
     public void integrationTestCarrito(){
