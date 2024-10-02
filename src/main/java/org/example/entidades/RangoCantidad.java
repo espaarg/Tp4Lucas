b19a8d97ab0c6941171b47c70b76c266bf0d02bf
Lucas Ortiz <123038170+espaarg@users.noreply.github.com>
2024-10-02 15:01:07 -0300

    Rango cantidad 2

--- src/main/java/org/example/entidades/RangoCantidad.java
+++ src/main/java/org/example/entidades/RangoCantidad.java
@@ -8,12 +8,14 @@ public class RangoCantidad {
     /**
      * Cantidad que se utiliza para verificar límites.
      */
-    int cant;
+    private int cant; // Cambiado a private
 
     private static final int MAX_CANTIDAD = 15; // Definido como constante
 
     /**
      * Este método nos asegura que la cantidad no supere la máxima.
+     * @return true si la cantidad es menor o igual a la máxima;
+     *         false en caso contrario.
      */
     public boolean cantidadMaxima() {
         return cant <= MAX_CANTIDAD;
@@ -21,15 +23,25 @@ public class RangoCantidad {
 
     /**
      * Este método nos asegura que la cantidad no sea negativa.
+     * @return true si la cantidad es mayor o igual a cero;
+     *         false en caso contrario.
      */
     public boolean cantidadMinima() {
         return cant >= 0;
     }
 
+    /**
+     * Obtiene la cantidad.
+     * @return la cantidad actual.
+     */
     public int getCant() {
         return cant;
     }
 
+    /**
+     * Establece la cantidad.
+     * @param cantidad la nueva cantidad a establecer.
+     */
     public void setCant(final int cantidad) {
         this.cant = cantidad;
     }
